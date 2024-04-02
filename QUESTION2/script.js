ec1cd9c8c23bc5472331a0174429ab91afa5ae89
david <david.mwangi2@student.moringaschool.com>
2024-04-02 21:28:37 +0300

    completed assignment

--- QUESTION2/script.js
+++ QUESTION2/script.js
@@ -1,15 +1,14 @@
 function generateArray(start, end) {
     let result = [];
-    if (start <= end) {
-        for (let i = start; i <= end; i++) {
-            result.push(i);
-        }
-    } else {
-        for (let i = start; i >= end; i--) {
-            result.push(i);
-        }
+    const step = start <= end ? 1 : -1;
+    for (let i = start; i !== end + step; i += step) {
+        result.push(i);
     }
     return result;
 }
-console.log(generateArray(4, 7)); 
-console.log(generateArray(-4, 7)); 
+function generateAndDisplayArray() {
+    const start = parseInt(document.getElementById('start').value);
+    const end = parseInt(document.getElementById('end').value);
+    const resultArray = generateArray(start, end);
+    document.getElementById('result').textContent = "Generated Array: " + resultArray.join(', ');
+}
